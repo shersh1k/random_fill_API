1abff1bfddfb8288bd40ddc733ff3060aee4f6ec
shersh1k <shershnev942@gmail.com>
2020-11-10 17:42:08 +0300

    config example

--- src/config/example.ts
+++ src/config/example.ts
@@ -2,3 +2,4 @@ export const REDIS_URL = process.env.REDIS_URL || "REDIS_URL";
 export const MONGO_DB = process.env.MONGO_DB_URL || 'MONGO_DB_URL';
 export const ioPath = '/socket';
 export const port = process.env.PORT || 4000;
+export const secret = 'random_fill';
