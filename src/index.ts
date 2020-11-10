c2fb2a1f1addf87425398632bff4ed83ec309b7c
shersh1k <shershnev942@gmail.com>
2020-11-10 11:23:52 +0300

    work with frontend

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 import * as express from 'express';
 import * as HTTP from 'http';
-import { Server } from 'socket.io';
+import * as cors from "cors";
+import { Server, Socket } from 'socket.io';
 
 const app = express();
 const http = HTTP.createServer(app);
-const io = new Server(http);
+const io = new Server(http, { path: '/socket' });
+
 const port = process.env.PORT || 4000;
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(cors({ credentials: true }));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-//   socket.broadcast.emit('hi');
+io.on('connection', (socket: Socket) => {
   socket.on('chat message', function (msg: string) {
     io.emit('chat message', msg);
   });
